0e419ff17f714539f03dafda0b58075b550142a4
Bryan Phelps <bryphe@outlook.com>
2018-10-08 19:29:40 -0700

    Add config to pick up include / lib

---
+++ src/config/discover.ml
@@ -0,0 +1,39 @@
+type os =
+    | Windows
+    | Mac
+    | Linux
+    | Unknown
+
+let uname () =
+    let ic = Unix.open_process_in "uname" in
+    let uname = input_line ic in
+    let () = close_in ic in
+    uname;;
+
+let get_os =
+    match Sys.os_type with
+    | "Win32" -> Windows
+    | _ -> match uname () with
+        | "Darwin" -> Mac
+        | "Linux" -> Linux
+        | _ -> Unknown
+
+let c_flags = ["-I";  (Sys.getenv "FREETYPE2_INCLUDE_PATH")]
+
+let ccopt s = ["-ccopt"; s]
+
+let flags = []
+    @ ccopt "-L."
+    @ ccopt ("-L" ^ (Sys.getenv "FREETYPE2_LIB_PATH"))
+    @ ccopt ("-lfreetype")
+;;
+
+let cxx_flags =
+    match get_os with
+    | Windows -> c_flags @ ["-fno-exceptions"; "-fno-rtti"; "-lstdc++"]
+    | _ -> c_flags
+;;
+
+Configurator.V1.Flags.write_sexp "c_flags.sexp" c_flags;
+Configurator.V1.Flags.write_sexp "cxx_flags.sexp" cxx_flags;
+Configurator.V1.Flags.write_sexp "flags.sexp" flags;
--- src/freetype.cpp
+++ src/freetype.cpp
@@ -2,13 +2,24 @@
 
 #include <caml/mlvalues.h>
 
-/* #include <ft2build.h> */
-/* #include FT_FREETYPE_H */
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 extern "C" {
     CAMLprim value
     caml_hello() {
-        printf("Hey from c");
+        printf("Hey from c, again");
+
+        FT_Library ft;
+        if (FT_Init_FreeType(&ft)) {
+            printf("ERROR INITING FREETYPE\n");
+        }
+
+        FT_Face face;
+        if (FT_New_Face(ft, "arial.ttf", 0, &face)) {
+            printf("Error loading font\n");
+        }
+
         return Val_unit;
     }
 }
